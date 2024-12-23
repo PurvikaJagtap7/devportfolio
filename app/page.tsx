425e118d9fca1941e3f7dda5ba574024ae4a0a34
Purvika Jagtap <purvikajagtap@gmail.com>
2024-12-23 20:35:04 +0530

    modify page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
 import Footer from "@/components/Footer";
 
-import Approach from "@/components/Approach";
 import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
@@ -21,7 +20,6 @@ const Home = () => {
         <RecentProjects />
 
         <Experience />
-        <Approach />
         <Footer />
       </div>
     </main>
